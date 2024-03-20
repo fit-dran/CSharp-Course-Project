03889042c3bd62a091f047be2a9d709a40e6c384
Adolfo <adolfo.luna@virginiogomez.cl>
2024-03-20 19:07:20 -0300

    interpolation and composite formatting chapters done

---
+++ CompositeFormating/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeFormatting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aba";
+            int age = 31;
+
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Name: " + name + "\nAge: " + age);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Your name is " + name + " and your age is " + age );
+
+            Console.WriteLine();
+
+            Console.WriteLine("Your name is {0} and your age is {1}", name,age);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Name: {0}\nAge: {1}", name, age);
+
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Interpolation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aba";
+            int age = 31;
+
+            Console.WriteLine("Your name is " + name + " and your age is " + age);
+
+            Console.WriteLine($"Your name is {name} and your age is {age}");
+
+            string test = string.Concat("Your name is ", name, " and your age is ", age);
+            Console.WriteLine(string.Concat("Your name is ", name, " and your age is ", age));
+
+            string[] names = new string[] { "Aba", "Test", "Array" };
+            Console.WriteLine(string.Concat(names));
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
